43fc2268c8a6500a998721af2b3b8f51cdfcfaff
ASG-SHOP-Team <farhan.fardid@asgshop.ai>
2025-08-08 16:33:46 +0600

    student dahsboard udpates and navabr updates

--- src/app/student/dashboard/page.js
+++ src/app/student/dashboard/page.js
@@ -15,56 +15,56 @@ import {
   Languages,
 } from "lucide-react";
 
-const subjects = [
-  {
-    name: "Physics",
-    progress: 0,
-    color: "bg-blue-500",
-    icon: Atom,
-    chaptersCompleted: 1,
-    totalChapters: 5,
-  },
-  {
-    name: "Chemistry",
-    progress: 0,
-    color: "bg-green-500",
-    icon: FlaskConical,
-    chaptersCompleted: 2,
-    totalChapters: 5,
-  },
-  {
-    name: "Mathematics",
-    progress: 0,
-    color: "bg-red-500",
-    icon: Calculator,
-    chaptersCompleted: 1,
-    totalChapters: 3,
-  },
-  {
-    name: "Biology",
-    progress: 0,
-    color: "bg-purple-500",
-    icon: HeartPulse,
-    chaptersCompleted: 2,
-    totalChapters: 3,
-  },
-  {
-    name: "Bangla",
-    progress: 0,
-    color: "bg-yellow-500",
-    icon: BookOpenText,
-    chaptersCompleted: 2,
-    totalChapters: 5,
-  },
-  {
-    name: "English",
-    progress: 0,
-    color: "bg-pink-500",
-    icon: Languages,
-    chaptersCompleted: 1,
-    totalChapters: 2,
-  },
-];
+// const subjects = [
+//   {
+//     name: "Physics",
+//     progress: 0,
+//     color: "bg-blue-500",
+//     icon: Atom,
+//     chaptersCompleted: 1,
+//     totalChapters: 5,
+//   },
+//   {
+//     name: "Chemistry",
+//     progress: 0,
+//     color: "bg-green-500",
+//     icon: FlaskConical,
+//     chaptersCompleted: 2,
+//     totalChapters: 5,
+//   },
+//   {
+//     name: "Mathematics",
+//     progress: 0,
+//     color: "bg-red-500",
+//     icon: Calculator,
+//     chaptersCompleted: 1,
+//     totalChapters: 3,
+//   },
+//   {
+//     name: "Biology",
+//     progress: 0,
+//     color: "bg-purple-500",
+//     icon: HeartPulse,
+//     chaptersCompleted: 2,
+//     totalChapters: 3,
+//   },
+//   {
+//     name: "Bangla",
+//     progress: 0,
+//     color: "bg-yellow-500",
+//     icon: BookOpenText,
+//     chaptersCompleted: 2,
+//     totalChapters: 5,
+//   },
+//   {
+//     name: "English",
+//     progress: 0,
+//     color: "bg-pink-500",
+//     icon: Languages,
+//     chaptersCompleted: 1,
+//     totalChapters: 2,
+//   },
+// ];
 
 function StudentDashboard() {
   const { data, isLoading, isError } = useGetMyCoursesQuery({
@@ -72,16 +72,15 @@ function StudentDashboard() {
     limit: 100,
   });
   const EnrolledCourses = data?.data?.data || [];
-  console.log("courseEnrolled", EnrolledCourses);
-  const calculateProgress = (completed, total) => {
-    return Math.round((completed / total) * 100);
-  };
+  // console.log("courseEnrolled", EnrolledCourses);
+  // const calculateProgress = (completed, total) => {
+  //   return Math.round((completed / total) * 100);
+  // };
   return (
     <div className="container mx-auto pt-28">
       <h1 className="text-2xl md:text-3xl font-bold mb-6 text-center">
-        My Courses
+        Enrolled Courses
       </h1>
-
       {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {subjects.map((subject, index) => {
           const Icon = subject.icon;
@@ -126,17 +125,31 @@ function StudentDashboard() {
           );
         })}
       </div> */}
+      {isLoading && (
+        <div className="text-center py-20 text-lg font-medium text-gray-600">
+          Loading courses ...
+        </div>
+      )}
 
-      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-4 md:py-8 p-4">
-        {EnrolledCourses.map((courseInfo, index) => (
-          <div key={index}>
-            <EnrolledCourseCard courseInfo={courseInfo} />
-          </div>
-        ))}
-      </div>
+      {(isError || (!isLoading && EnrolledCourses.length === 0)) && (
+        <div className="text-center py-20 text-lg font-medium text-red-500">
+          Failed to load courses info, please try again
+        </div>
+      )}
+
+      {!isLoading && !isError && EnrolledCourses.length > 0 && (
+        <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-4 md:py-8 p-4">
+          {EnrolledCourses.map((courseInfo, index) => (
+            <div key={index}>
+              <EnrolledCourseCard courseInfo={courseInfo} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
+
 export default function StudentProfile() {
   return (
     <StudentRoute>
