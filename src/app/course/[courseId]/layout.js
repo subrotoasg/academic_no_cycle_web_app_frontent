9cce58c888d0e16dcef897ab7126ed8757f6089d
ASG-SHOP-Team <farhan.fardid@asgshop.ai>
2025-08-19 11:17:45 +0600

    course layout and mycourses updates

--- src/app/course/[courseId]/layout.js
+++ src/app/course/[courseId]/layout.js
@@ -15,9 +15,6 @@ function ActualLayoutLogic({ children }) {
 
   const user = useSelector(currentUser);
   const role = user?.role;
-  // const enrolledCourses = useSelector(
-  //   (state) => state.studentCourses.enrolledCourses
-  // );
 
   const enrolledCourses = useSelector(selectEnrolledCourses);
 
@@ -30,11 +27,6 @@ function ActualLayoutLogic({ children }) {
 
   // Admin data
   const teacherCourses = useSelector(selectAllCourses);
-  // console.log(teacherCourses);
-
-  // const isEnrolledOrArchiveAllowed =
-  //   courses?.some((c) => c.courseId === courseId) ||
-  //   allowedArchives.includes(courseId);
 
   // Check access
   const isEnrolledOrArchiveAllowed =
@@ -50,16 +42,11 @@ function ActualLayoutLogic({ children }) {
       allowedArchives.includes(courseId));
 
   useEffect(() => {
-    // if (!isLoading && !isEnrolledOrArchiveAllowed) {
     if (!isLoading && !(isEnrolledOrArchiveAllowed || isTeacherAllowed)) {
       router.replace("/unauthorized");
     }
-    // }, [isLoading, isEnrolledOrArchiveAllowed, router]);
   }, [isLoading, isEnrolledOrArchiveAllowed, isTeacherAllowed, router]);
 
-  // if (!isEnrolledOrArchiveAllowed) {
-  //   return null;
-  // }
   if (!isEnrolledOrArchiveAllowed && !isTeacherAllowed) {
     return null;
   }
